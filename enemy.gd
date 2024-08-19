1dfc8fc66bbfec45533f2cd06e027125dfba1c09
OhijMik <jihok148@gmail.com>
2024-08-18 20:57:28 -0400

    Added the fog effect

--- enemy.gd
+++ enemy.gd
@@ -1,8 +1,6 @@
 extends CharacterBody3D
 
 @onready var armature = get_node("Armature")
-@onready var spring_arm_pivot = get_node("SpringArmPivot")
-@onready var spring_arm = get_node("SpringArmPivot/SpringArm3D")
 @onready var anim_tree = get_node("AnimationTree")
 @onready var player = get_node("../../Player")
 
@@ -18,30 +16,13 @@ func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 
-func _unhandled_input(event):
-	if Input.is_action_just_pressed("quit"):
-		get_tree().quit()
-		
-	if event is InputEventMouseMotion:
-		spring_arm_pivot.rotate_y(-event.relative.x * .005)
-		spring_arm.rotate_x(-event.relative.y * .005)
-		spring_arm.rotation.x = clamp(spring_arm.rotation.x, -PI/4, PI/4)
-
-
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
-	# Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Vector3.ZERO
-
-	var direction = (transform.basis * Vector3(player.position.x - position.x, 0, player.position.z - position.z)).normalized()
+	var direction = Vector3.ZERO
+	# direction = (transform.basis * Vector3(player.position.x - position.x, 0, player.position.z - position.z)).normalized()
 	
 	if direction:
 		velocity.x = lerp(velocity.x, direction.x * SPEED, lerp_val)
