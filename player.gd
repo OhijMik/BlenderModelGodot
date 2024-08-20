053fe3d7e36ed9b830aabb0962c0438c196a172f
OhijMik <jihok148@gmail.com>
2024-08-19 20:47:13 -0400

    Added the flashlight
    -working on making the flashlight reveal objects outside of the fog

--- player.gd
+++ player.gd
@@ -4,6 +4,7 @@ extends CharacterBody3D
 @onready var spring_arm_pivot = get_node("SpringArmPivot")
 @onready var spring_arm = get_node("SpringArmPivot/SpringArm3D")
 @onready var anim_tree = get_node("AnimationTree")
+@onready var flashlight = get_node("../DirectionalLight3D")
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
@@ -31,10 +32,13 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
+	
+	flashlight.rotation.y = spring_arm_pivot.rotation.y + 135
 
-	# Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
+	if Input.is_action_pressed("Flashlight"):
+		flashlight.light_energy = 0.01
+	else:
+		flashlight.light_energy = 0
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
