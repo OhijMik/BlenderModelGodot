e55191ff6490d3968a19bccdc245ef350f0387fa
OhijMik <jihok148@gmail.com>
2024-08-19 21:50:20 -0400

    Added the enemy agro

--- enemy.gd
+++ enemy.gd
@@ -3,7 +3,7 @@ extends CharacterBody3D
 @onready var armature = get_node("Armature")
 @onready var anim_tree = get_node("AnimationTree")
 @onready var player = get_node("../../Player")
-@onready var timer = get_node("AgroTimer")
+@onready var agro_timer = get_node("AgroTimer")
 
 const SPEED = 3.0
 
@@ -11,6 +11,7 @@ const lerp_val = 0.15
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var agro = false
 
 
 func _ready():
@@ -18,10 +19,22 @@ func _ready():
 
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
+	if not agro:
+		if agro_timer.is_stopped():
+			follow_player()
+			if position.distance_to(player.position) <= 4:
+				agro_timer.start()
+		else:
+			velocity = Vector3.ZERO
+	else:
+		follow_player()
+	
+	anim_tree.set("parameters/BlendSpace1D/blend_position", velocity.length() / SPEED)
+	print(agro_timer.time_left)
+	move_and_slide()
 
+
+func follow_player():
 	var direction = Vector3.ZERO
 	direction = (transform.basis * Vector3(player.position.x - position.x, 0, player.position.z - position.z)).normalized()
 	
@@ -33,10 +46,10 @@ func _physics_process(delta):
 	else:
 		velocity.x = lerp(velocity.x, 0.0, lerp_val)
 		velocity.z = lerp(velocity.z, 0.0, lerp_val)
-	
-	if position.distance_to(player.position) <= 2 and not timer.is_playing():
-		velocity = Vector3.ZERO
-	
-	anim_tree.set("parameters/BlendSpace1D/blend_position", velocity.length() / SPEED)
 
-	move_and_slide()
+
+func _on_agro_timer_timeout():
+	if position.distance_to(player.position) <= 4:
+		agro = true
+	else:
+		agro_timer.stop()
