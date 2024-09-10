3f84e128bd961540c31e5c26d9e9d506369db58b
OhijMik <jihok148@gmail.com>
2024-09-09 21:19:53 -0400

    Changed player pathing and added blink effect

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -7,6 +7,7 @@ extends CharacterBody3D
 @onready var deaggro_timer = get_node("DeaggroTimer")
 @onready var passive_timer = get_node("PassiveTimer")
 @onready var move_timer = get_node("MoveTimer")
+@onready var player_follow_timer = get_node("PlayerFollowTimer")
 
 const lerp_val = 0.15
 
@@ -40,9 +41,9 @@ func _ready():
 
 func _physics_process(delta):
 	if madness <= 4:
-		$"../UI/MadnessLabel".text = "Madness: " + str(madness)
+		$"../UI/Madness/MadnessLabel".text = "Madness: " + str(madness)
 	else:
-		$"../UI/MadnessLabel".text = "Madness: MAX"
+		$"../UI/Madness/MadnessLabel".text = "Madness: MAX"
 	
 	# Enemy is passive
 	if passive:
@@ -53,21 +54,37 @@ func _physics_process(delta):
 	elif not aggro:
 		$Armature/Skeleton3D/OmniLight3D.light_color = Color(255, 255, 255)
 		speed = non_aggro_speed
+
 		# Player is near
 		if aggro_timer.is_stopped() and position.distance_to(player.position) <= 12 and \
 				not player.under_table:
-			follow_target(player.position)
 			move_timer.stop()
+			# Start the player follow timer
+			if player_follow_timer.is_stopped():
+				get_pos_path(player.position)
+				player_follow_timer.start()
+			
+			# Get the path to the player and follow the player
+			if path.size() > 0:
+				if path_idx >= path.size():
+					pass
+				elif position.distance_to(path[path_idx]) < 1:
+					path_idx += 1
+				else:
+					follow_target(path[path_idx])
+			
 			# Start the aggro timer if the player is too close
 			if position.distance_to(player.position) <= 5:
 				aggro_timer.start()
 		elif not aggro_timer.is_stopped():
 			velocity = Vector3.ZERO
+			player_follow_timer.stop()
 		else:		# Player is not near
+			player_follow_timer.stop()
 			if path.size() > 0:
 				if path_idx >= path.size():
 					pass
-				elif global_transform.origin.distance_to(path[path_idx]) < 1:
+				elif position.distance_to(path[path_idx]) < 1:
 					path_idx += 1
 				else:
 					if nav.is_target_reached() and move_timer.is_stopped():		# Target location is reached
@@ -94,8 +111,17 @@ func _physics_process(delta):
 			# Kill player if the player is under the table
 			if player.under_table and position.distance_to(player.position) <= 4:
 				player.dead = true
-		follow_target(player.global_position)
+		
+		# Get the path to the player and follow the player
+		if path.size() > 0:
+			if path_idx >= path.size():
+				pass
+			elif position.distance_to(path[path_idx]) < 1:
+				path_idx += 1
+			else:
+				follow_target(path[path_idx])
 	
+	# Only show the lights if the player is close
 	if position.distance_to(player.position) <= 7:
 		$Armature/Skeleton3D/OmniLight3D.light_energy = 1
 	else:
@@ -120,95 +146,95 @@ func follow_target(target_pos):
 		velocity.z = lerp(velocity.z, 0.0, lerp_val)
 
 
-func get_room_path(room_pos):
-	nav.set_target_position(room_pos)
-	path = NavigationServer3D.map_get_path($NavigationAgent3D/NavigationRegion3D.get_navigation_map(), global_transform.origin, room_pos, true)
+func get_pos_path(pos):
+	nav.set_target_position(pos)
+	path = NavigationServer3D.map_get_path($NavigationAgent3D/NavigationRegion3D.get_navigation_map(), global_transform.origin, pos, true)
 	path_idx = 0
 
 
 func get_target_room():
 	if cur_room == 1:
 		target_room = 3		# Go to room 3
-		get_room_path(Vector3(0, 0, 0))
+		get_pos_path(Vector3(0, 0, 0))
 	elif cur_room == 2:
 		target_room = 3		# Go to room 3
-		get_room_path(Vector3(0, 0, 0))
+		get_pos_path(Vector3(0, 0, 0))
 	elif cur_room == 3:
 		var rand_num = rng.randi_range(0, 3)
 		if rand_num == 0:	# Go to room 1
 			target_room = 1
-			get_room_path(Vector3(-14.8, 0, 5.9))
+			get_pos_path(Vector3(-14.8, 0, 5.9))
 		elif rand_num == 1:	# Go to room 2
 			target_room = 2
-			get_room_path(Vector3(-14.8, 0, -4.5))
+			get_pos_path(Vector3(-14.8, 0, -4.5))
 		elif rand_num == 2:	# Go to hall 1
 			target_room = 10
-			get_room_path(Vector3(3, 0, 14.1))
+			get_pos_path(Vector3(3, 0, 14.1))
 		else:				# Go to hall 2
 			target_room = 11
-			get_room_path(Vector3(18, 0, -2.26))
+			get_pos_path(Vector3(18, 0, -2.26))
 	elif cur_room == 4:
 		target_room = 5		# Go to room 5
-		get_room_path(Vector3(24.1, 0, 9.4))
+		get_pos_path(Vector3(24.1, 0, 9.4))
 	elif cur_room == 5:
 		var rand_num = rng.randi_range(0, 2)
 		if rand_num == 0:	# Go to hall 2
 			target_room = 11
-			get_room_path(Vector3(18, 0, -2.26))
+			get_pos_path(Vector3(18, 0, -2.26))
 		elif rand_num == 1:	# Go to room 4
 			target_room = 4
-			get_room_path(Vector3(31.4, 0, -1))
+			get_pos_path(Vector3(31.4, 0, -1))
 		else:				# Go to room 6
 			target_room = 6
-			get_room_path(Vector3(27, 0, 28))
+			get_pos_path(Vector3(27, 0, 28))
 	elif cur_room == 6:
 		var rand_num = rng.randi_range(0, 1)
 		if rand_num == 0:	# Go to room 5
 			target_room = 5
-			get_room_path(Vector3(24.1, 0, 9.4))
+			get_pos_path(Vector3(24.1, 0, 9.4))
 		else:				# Go to room 7
 			target_room = 7
-			get_room_path(Vector3(1.8, 0, 28.3))
+			get_pos_path(Vector3(1.8, 0, 28.3))
 	elif cur_room == 7:
 		var rand_num = rng.randi_range(0, 3)
 		if rand_num == 0:	# Go to room 6
 			target_room = 6
-			get_room_path(Vector3(27, 0, 28))
+			get_pos_path(Vector3(27, 0, 28))
 		elif rand_num == 1:	# Go to room 8
 			target_room = 8
-			get_room_path(Vector3(-0.9, 0, 40.1))
+			get_pos_path(Vector3(-0.9, 0, 40.1))
 		elif rand_num == 2:	# Go to room 9
 			target_room = 9
-			get_room_path(Vector3(16.1, 0, 32.5))
+			get_pos_path(Vector3(16.1, 0, 32.5))
 		else:				# Go to hall 1
 			target_room = 10
-			get_room_path(Vector3(3, 0, 14.1))
+			get_pos_path(Vector3(3, 0, 14.1))
 	elif cur_room == 8:
 		target_room = 7		# Go to room 7
-		get_room_path(Vector3(1.8, 0, 28.3))
+		get_pos_path(Vector3(1.8, 0, 28.3))
 	elif cur_room == 9:
 		target_room = 7		# Go to room 7
-		get_room_path(Vector3(1.8, 0, 28.3))
+		get_pos_path(Vector3(1.8, 0, 28.3))
 	elif cur_room == 10:
 		var rand_num = rng.randi_range(0, 1)
 		if rand_num == 0:	# Go to room 3
 			target_room = 3
-			get_room_path(Vector3(0, 0, 0))
+			get_pos_path(Vector3(0, 0, 0))
 		else:				# Go to room 7
 			target_room = 7
-			get_room_path(Vector3(1.8, 0, 28.3))
+			get_pos_path(Vector3(1.8, 0, 28.3))
 	else:
 		var rand_num = rng.randi_range(0, 1)
 		if rand_num == 0:	# Go to room 3
 			target_room = 3
-			get_room_path(Vector3(0, 0, 0))
+			get_pos_path(Vector3(0, 0, 0))
 		else:				# Go to room 5
 			target_room = 5
-			get_room_path(Vector3(24.1, 0, 9.4))
+			get_pos_path(Vector3(24.1, 0, 9.4))
 
 
 func madness_check():
-	$"../UI/MadnessProgressBar".value = 0
+	$"../UI/Madness/MadnessProgressBar".value = 0
 	if madness == 1:
 		aggro_timer.wait_time = 2.5
 		deaggro_timer.wait_time = 2.5
@@ -222,7 +248,7 @@ func madness_check():
 		
 		non_aggro_speed = 3.0
 		aggro_speed = 8.0
-		$"../UI/MadnessProgressBar".max_value = 20
+		$"../UI/Madness/MadnessProgressBar".max_value = 20
 		$"../WorldEnvironment".environment.fog_density = 0.5
 	elif madness == 2:
 		aggro_timer.wait_time = 2.0
@@ -236,7 +262,7 @@ func madness_check():
 		long_move_time_max = 5.0
 		
 		non_aggro_speed = 3.5
-		$"../UI/MadnessProgressBar".max_value = 20
+		$"../UI/Madness/MadnessProgressBar".max_value = 20
 		$"../WorldEnvironment".environment.fog_density = 0.6
 	elif madness == 3:
 		aggro_timer.wait_time = 1.5
@@ -251,7 +277,7 @@ func madness_check():
 		
 		non_aggro_speed = 4.5
 		aggro_speed = 8.5
-		$"../UI/MadnessProgressBar".max_value = 15
+		$"../UI/Madness/MadnessProgressBar".max_value = 15
 		$"../WorldEnvironment".environment.fog_density = 0.7
 	elif madness == 4:
 		aggro_timer.wait_time = 1.0
@@ -266,7 +292,7 @@ func madness_check():
 		
 		non_aggro_speed = 5.0
 		aggro_speed = 9.0
-		$"../UI/MadnessProgressBar".max_value = 15
+		$"../UI/Madness/MadnessProgressBar".max_value = 15
 		$"../WorldEnvironment".environment.fog_density = 0.8
 	else:
 		aggro_timer.wait_time = 0.1
@@ -282,7 +308,7 @@ func madness_check():
 		non_aggro_speed = 6.0
 		aggro_speed = 15.0
 		aggro = true
-		$"../UI/MadnessProgressBar".value = 15
+		$"../UI/Madness/MadnessProgressBar".value = 15
 		$"../WorldEnvironment".environment.fog_density = 1
 
 
@@ -290,6 +316,8 @@ func _on_aggro_timer_timeout():
 	# Player is too close
 	if position.distance_to(player.position) <= 8 and not player.under_table:
 		aggro = true
+		get_pos_path(player.global_position)
+		player_follow_timer.start()
 	else:
 		get_target_room()
 		aggro_timer.stop()
@@ -297,6 +325,7 @@ func _on_aggro_timer_timeout():
 
 func _on_deaggro_timer_timeout():
 	aggro = false
+	player_follow_timer.stop()
 	passive_timer.start()
 	passive = true
 
@@ -316,3 +345,6 @@ func _on_madness_timer_timeout():
 		madness += 1
 		madness_check()
 
+
+func _on_player_follow_timer_timeout():
+	get_pos_path(player.global_position)
--- Scripts/main_scene.gd
+++ Scripts/main_scene.gd
@@ -1,6 +1,9 @@
 extends Node
 
 @onready var player = get_node("Player")
+@onready var enemy = get_node("Enemy")
+
+@onready var black_screen_wait_timer = get_node("UI/BlackScreen/BlackScreenWaitTimer")
 
 
 func _process(delta):
@@ -11,8 +14,31 @@ func _process(delta):
 		get_tree().change_scene_to_file("res://Scenes/win_scene.tscn")
 	
 	if player.dead:
+		$UI/BlackScreen/BlackScreenLingeringTimer.stop()
+		black_screen_wait_timer.stop()
 		get_tree().change_scene_to_file("res://Scenes/death_scene.tscn")
+	
+	if player.position.distance_to(enemy.position) <= 2:
+		black_screen_wait_timer.wait_time = 0.1
+	elif player.position.distance_to(enemy.position) <= 3:
+		black_screen_wait_timer.wait_time = 0.25
+	elif player.position.distance_to(enemy.position) <= 5:
+		black_screen_wait_timer.wait_time = 0.4
+	elif player.position.distance_to(enemy.position) <= 6:
+		black_screen_wait_timer.wait_time = 0.6
+	elif player.position.distance_to(enemy.position) <= 7:
+		black_screen_wait_timer.wait_time = 0.8
+	
+	if player.position.distance_to(enemy.position) <= 7 and \
+			black_screen_wait_timer.is_stopped():
+		$UI/BlackScreen/BlackScreen.show()
+		$UI/BlackScreen/BlackScreenLingeringTimer.start()
+		black_screen_wait_timer.start()
 
 
 func _on_madness_inc_timer_timeout():
-	$UI/MadnessProgressBar.value += 1
+	$UI/Madness/MadnessProgressBar.value += 1
+
+
+func _on_black_screen_lingering_timer_timeout():
+	$UI/BlackScreen/BlackScreen.hide()
